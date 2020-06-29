544e596512d4e56909bfe9b2494a7fdd24a70d36
walacefex <walacefex01@gmail.com>
2020-06-28 22:52:54 -0300

    Criando o sistema de validação do formulário

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,31 +1,70 @@
 import React, { Component } from 'react';
-
+import FormValidator from './FormValidator';
+import PopUp from './Popup';
 
 class Formulario extends Component{
 
     constructor(props){
         super(props);
 
+        this.validador = new FormValidator([
+            {
+              campo: 'nome',
+              metodo: 'isEmpty',
+              validoQuando: false,
+              mensagem: 'Entre com um nome'
+            },
+            { 
+              campo: 'livro',
+              metodo: 'isEmpty',
+              validoQuando: false,
+              mensagem: 'Entre com um livro'
+            },
+            { 
+                campo: 'preco',
+                metodo: 'isInt',
+                args: [{min: 0, max: 99999}],
+                validoQuando: true,
+                mensagem: 'Entre com um valor numérico'
+              }
+          ]);
+
         this.stateInicial = {
             nome: '',
             livro:'',
             preco: '',
+            validacao: this.validador.valido()
         }
         this.state = this.stateInicial;
     }
-    //atualizar estado do input
-    escutadorDeInput = event =>{
-        const { name, value  } = event.target;
+   //atualizar estado do input
+   escutadorDeInput = event =>{
+    const { name, value  } = event.target;
 
-        this.setState({
-            [name]: value
-        });
-    }
-
+    this.setState({
+        [name]: value
+    });
+}
     submitFormulario = () =>{
+    const validacao = this.validador.valida(this.state);
+
+    if(validacao.isValid){
         this.props.escutadorDeSubmit(this.state);
         this.setState(this.stateInicial);
+    }else{
+        const {nome, livro, preco} = validacao;
+        const campos = [nome, livro, preco];
+
+        const camposInvalidos = campos.filter(elem => {
+            return elem.isInvalid;
+        });
+        camposInvalidos.forEach(campo =>{
+            PopUp.exibeMensagem('error', campo.message);
+        })
+        }
+       
     }
+     
 
     render(){
 
