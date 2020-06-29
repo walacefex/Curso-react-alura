ebc6d91abdc528c3a8ce1f01202bfef9f9c7091e
walacefex <walacefex01@gmail.com>
2020-06-28 22:56:32 -0300

    Chamando as páginas criadas e implementando o esquema de rotas do react

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Sobre from './Sobre';
+import Livros from './Livros';
+import Autores from './Autores';
+import NotFound from './NotFound';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <BrowserRouter>
+      <Switch>
+          <Route path='/' exact={true} component={App}/>
+          <Route path='/sobre' component={Sobre}/>
+          <Route path='/livros' component={Livros}/>
+          <Route path='/autores' component={Autores}/>
+          <Route component={NotFound}/>
+      </Switch>
+  </BrowserRouter>
+,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
