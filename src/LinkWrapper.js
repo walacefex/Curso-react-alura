4b370f8e684563d47f82f665ddb85c8ad066581c
walacefex <walacefex01@gmail.com>
2020-06-28 23:00:02 -0300

    Criando arquivo para validar as rotas, para não ficar chamando LINK nas rotas desejadas

---
+++ src/LinkWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const LinkWrapper = props =>{
+  return(
+    <NavLink activeStyle={{ fontWeight: "bold"}} {...props}/>
+  );
+}
+
+
+export default LinkWrapper;
