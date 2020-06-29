ccaed125ac547490a88ff8d54727c97b669fc77f
walacefex <walacefex01@gmail.com>
2020-06-28 23:01:17 -0300

    Pagina nao encontrada

---
+++ src/NotFound.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Header  from './Header';
+
+
+const NotFound = () =>{
+  return(
+    <Fragment>
+      <Header/>
+    <h1>Página não encontrada</h1>
+    </Fragment>
+  )
+}
+
+
+export default NotFound;
